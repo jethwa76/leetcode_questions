e5922d92d5ebd1e5516c9f79e1228047d52efb90
JETHWA HARSH YATINKUMAR <harshyjethwa2020@gmail.com>
2025-12-26 18:31:20 +0530

    Create 30.java

---
+++ 30.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        if (s == null || words == null || words.length == 0) return result;
+        int wordLen = words[0].length();
+        int wordCount = words.length;
+        int totalLen = wordLen * wordCount;
+        if (s.length() < totalLen) return result;
+        Map<String, Integer> wordMap = new HashMap<>();
+        for (String w : words) {
+            wordMap.put(w, wordMap.getOrDefault(w, 0) + 1);
+        }
+        for (int i = 0; i < wordLen; i++) {
+            int left = i, right = i;
+            int count = 0;
+            Map<String, Integer> windowMap = new HashMap<>();
+            while (right + wordLen <= s.length()) {
+                String word = s.substring(right, right + wordLen);
+                right += wordLen;
+                if (wordMap.containsKey(word)) {
+                    windowMap.put(word, windowMap.getOrDefault(word, 0) + 1);
+                    count++;
+                    while (windowMap.get(word) > wordMap.get(word)) {
+                        String leftWord = s.substring(left, left + wordLen);
+                        windowMap.put(leftWord, windowMap.get(leftWord) - 1);
+                        left += wordLen;
+                        count--;
+                    }
+                    if (count == wordCount) {
+                        result.add(left);
+                    }
+                } else {
+                    windowMap.clear();
+                    count = 0;
+                    left = right;
+                }
+            }
+        }
+        return result;
+    }
+}
