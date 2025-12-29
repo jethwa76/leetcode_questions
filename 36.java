2f5b8af1907c5e232b2ee85275eaf17b5fb02fda
JETHWA HARSH YATINKUMAR <harshyjethwa2020@gmail.com>
2025-12-29 14:20:22 +0530

    Create 36.java

---
+++ 36.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] rows = new HashSet[9];
+        HashSet<Character>[] cols = new HashSet[9];
+        HashSet<Character>[] boxes = new HashSet[9];
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char ch = board[i][j];
+                if (ch == '.') continue;
+                if (rows[i].contains(ch)) return false;
+                rows[i].add(ch);
+                if (cols[j].contains(ch)) return false;
+                cols[j].add(ch);
+                int boxIndex = (i / 3) * 3 + (j / 3);
+                if (boxes[boxIndex].contains(ch)) return false;
+                boxes[boxIndex].add(ch);
+            }
+        }
+        return true;
+    }
+}
